f99906ea20689cc70d7e382a1d2125a18530f953
Sergey Tyapkin <tyapkin2002@mail.ru>
2025-07-04 12:35:45 +0300

    Initial commit

---
+++ src/components/loaders/PointsLoading.vue
@@ -0,0 +1,62 @@
+<style lang="stylus" scoped>
+@import '../../styles/constants.styl'
+@import '../../styles/components.styl'
+@import '../../styles/buttons.styl'
+@import '../../styles/fonts.styl'
+@import '../../styles/utils.styl'
+@import '../../styles/animations.styl'
+@import '../../styles/scrollbars.styl'
+
+.circle-loading
+  width calc(var(--size) / 4)
+  height calc(var(--size) / 4)
+  margin-top calc(var(--size) / 4 * 1.5)
+  margin-bottom calc(var(--size) / 4 * 1.5)
+  position relative
+  border-radius 50%
+  color colorEmp1
+  animation opacity-in 0.4s linear forwards, fill 1s ease-in infinite alternate
+  &::before
+  &::after
+    content ''
+    position absolute
+    height 100%
+    width 100%
+    border-radius 50%
+    left calc(var(--size) / 4 * 1.5)
+    top 0
+    animation fill 0.9s ease-in infinite alternate
+
+  &::after
+    left auto
+    right calc(var(--size) / 4 * 1.5)
+    animation-duration 1.1s
+
+  @keyframes opacity-in
+    0%
+      opacity 0
+    100%
+      opacity 1
+
+  @keyframes fill
+    0%
+      box-shadow 0 0 0 -1px inset
+    100%
+      box-shadow 0 0 0 calc(var(--size) / 8) inset
+</style>
+
+<template>
+  <div class="circle-loading" :style="{ '--size': size }" :class="{centered}" />
+</template>
+
+<script lang="ts">
+export default {
+  props: {
+    size: {
+      type: String,
+      default: '60px',
+    },
+    centered: Boolean,
+  },
+};
+</script>
---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { createApp } from 'vue';
+
+import App from '~/App.vue';
+import WS from '@sergtyapkin/reconnecting-websocket';
+import Store from '~/Store';
+import createVueRouter from '~/Router';
+
+import '~/styles/fontsLoader.styl';
+import '~/styles/global.styl';
+import swAPI from '~/serviceWorker/swAPI';
+
+await swAPI.register();
+
+const Router = createVueRouter(Store);
+const app = createApp(App).use(Router).use(WS).use(Store).mount('#app');
+Store.$app = app;
