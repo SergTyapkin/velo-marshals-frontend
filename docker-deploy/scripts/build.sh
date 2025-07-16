563366b98c3ec0892ef0ca3dbbcd3d9396c96236
Сергей Тяпкин <Tyapkin2002@mail.ru>
2025-07-16 17:08:10 +0300

    fix build.sh

--- docker-deploy/scripts/build.sh
+++ docker-deploy/scripts/build.sh
@@ -1,15 +1,15 @@
 enable_https="$(. "./.env"; echo "$VITE_HTTPS" | tr -dc "a-zA-Z")" &&
 if [[ $enable_https == "true" ]] || [[ $enable_https == "True" ]] || [[ $enable_https == "TRUE" ]]
     then
-      HTTP_NGINX_CONFIG="
+      HTTP_NGINX_CONFIG=$'
   listen 80;
   listen [::]:80;
   server_name ${DOMAIN_URL};
 
   include /etc/nginx/conf.d/locations/http/*.conf;
 
-  return 301 https://$host$request_uri;"
-      HTTPS_NGINX_CONFIG="
+  return 301 https://$host$request_uri;'
+      HTTPS_NGINX_CONFIG=$'
   listen       443 ssl default_server;
   listen       [::]:443 ssl;
   server_name ${DOMAIN_URL};
@@ -21,17 +21,17 @@ if [[ $enable_https == "true" ]] || [[ $enable_https == "True" ]] || [[ $enable_
   ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA';
   ssl_prefer_server_ciphers on;
 
-  include /etc/nginx/conf.d/locations/https/*.conf;"
+  include /etc/nginx/conf.d/locations/https/*.conf;'
     else
-      HTTP_NGINX_CONFIG="
+      HTTP_NGINX_CONFIG=$'
   listen 80;
   listen [::]:80;
   server_name ${DOMAIN_URL};
 
   include /etc/nginx/conf.d/locations/http/*.conf;
 
-  include /etc/nginx/conf.d/locations/https/*.conf;"
-      HTTPS_NGINX_CONFIG=""
+  include /etc/nginx/conf.d/locations/https/*.conf;'
+      HTTPS_NGINX_CONFIG=$''
 fi &&
 
 cd ./docker-deploy &&
